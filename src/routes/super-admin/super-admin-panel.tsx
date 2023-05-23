3666416b98281454a0909cab4814f62d41f670f3
Sergii Liuk <sergiiliuk@yahoo.dk>
2023-05-23 11:00:09 +0200

    feat: admin-panel- register new user

--- src/routes/super-admin/super-admin-panel.tsx
+++ src/routes/super-admin/super-admin-panel.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { TabHeader } from "../../components/details-header";
+import ErrorBoundary from "../../components/error-boundary";
 import { PageContainer } from "../../components/page-container";
-import RegisterUser from "./register-user";
+import { Spinner } from "../../components/spinner";
+import { AdminNavigationTabs } from "./admin-navigation-tabs";
+import { RegisterUser } from "./register-user";
 
-export const SuperAdminPanel = () => {
+export default function SuperAdminPanel() {
   return (
     <PageContainer>
-      <RegisterUser />
+      <div>
+        <div className="border-b pt-2 border-cyan-800">
+          {<AdminNavigationTabs />}
+        </div>
+        <TabHeader>
+          <div className="flex-1"> </div>
+          <div className="flex gap-2">
+            <RegisterUser />
+          </div>
+        </TabHeader>
+        <ErrorBoundary>
+          <React.Suspense fallback={<Spinner />}>{<Outlet />}</React.Suspense>
+        </ErrorBoundary>
+      </div>
     </PageContainer>
   );
-};
+}
