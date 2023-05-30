141900805c873fd20558351d30e2faf66efbf4d9
Sergii Liuk <sergiiliuk@yahoo.dk>
2023-05-30 21:47:43 +0200

    test uri

--- src/utils/apollo-client.ts
+++ src/utils/apollo-client.ts
@@ -3,7 +3,8 @@ import { setContext } from "@apollo/client/link/context";
 import { useAuthToken } from "../hooks/useAuthToken ";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000/graphql",
+  // uri: "http://localhost:5000/graphql",
+  uri:"https://comfy-conkies-72d572.netlify.app/
 });
 
 const authLink = setContext((_, { headers }) => {
