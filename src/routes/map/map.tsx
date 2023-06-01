9a8d5c4ae7f21a627ee8f9f98b23ba1a5b249ee5
Sergii Liuk <sergiiliuk@yahoo.dk>
2023-06-01 09:38:17 +0200

    feat: map libre init

--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Map as MapGL, NavigationControl } from "react-map-gl";
+import { PageContainer } from "../../components/page-container";
+import maplibregl from "maplibre-gl";
 
 export const Map = () => {
-  return <div>Map</div>;
+  return (
+    <PageContainer>
+      <MapGL
+        mapLib={maplibregl}
+        initialViewState={{
+          longitude: 9.924400733409513,
+          latitude: 55.818065069402195,
+          zoom: 15,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle={`https://api.maptiler.com/maps/basic-v2/style.json?key=P5R8144Hukr6UEEJm5NV`}
+      >
+        <NavigationControl position="top-left" />
+      </MapGL>
+    </PageContainer>
+  );
 };
