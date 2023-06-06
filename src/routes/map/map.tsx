043eefdd94b84cedbd03f75e9102633e501f4bd4
Sergii Liuk <sergiiliuk@yahoo.dk>
2023-06-06 20:36:01 +0200

    feat: map width (WIP)

--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Map as MapGL, NavigationControl } from "react-map-gl";
 import { PageContainer } from "../../components/page-container";
 import maplibregl from "maplibre-gl";
 
 export const Map = () => {
+  const [width, setWidth] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!ref.current) return;
+    setWidth(ref.current.offsetWidth);
+  });
+
   return (
     <PageContainer>
-      <MapGL
-        mapLib={maplibregl}
-        initialViewState={{
-          longitude: 9.924400733409513,
-          latitude: 55.818065069402195,
-          zoom: 15,
-        }}
-        style={{ width: 600, height: 400 }}
-        mapStyle={`https://api.maptiler.com/maps/basic-v2/style.json?key=P5R8144Hukr6UEEJm5NV`}
-      >
-        <NavigationControl position="top-left" />
-      </MapGL>
+      <div ref={ref}>
+        <MapGL
+          mapLib={maplibregl}
+          initialViewState={{
+            longitude: 9.924400733409513,
+            latitude: 55.818065069402195,
+            zoom: 15,
+          }}
+          style={{ width: width, height: 400 }}
+          mapStyle={`https://api.maptiler.com/maps/basic-v2/style.json?key=P5R8144Hukr6UEEJm5NV`}
+        >
+          <NavigationControl position="top-left" />
+        </MapGL>
+      </div>
     </PageContainer>
   );
 };
