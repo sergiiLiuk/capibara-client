3262a8d3d4b122c9bba63046d8dc0157c51078f4
Sergii Liuk <sergiiliuk@yahoo.dk>
2023-06-12 14:48:42 +0200

    fix: maplible style import

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./app";
 import client from "./services/apollo-client";
 import { AuthProvider } from "./context/auth-context";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -1,14 +1,8 @@
 import maplibregl from "maplibre-gl";
 import React, { useLayoutEffect, useRef, useState } from "react";
-import {
-  Layer,
-  LayerProps,
-  Map as MapGL,
-  NavigationControl,
-  Source,
-} from "react-map-gl";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { Layer, LayerProps, Map as MapGL, Marker, Source } from "react-map-gl";
 import { PageContainer } from "../../components/page-container";
-
 import { fieldData } from "./field-test";
 
 const polygonStyle: LayerProps = {
@@ -48,18 +42,24 @@ export const Map = () => {
         <MapGL
           mapLib={maplibregl}
           initialViewState={{
-            longitude: 9.924400733409513,
+            longitude: 9.914400733409513,
             latitude: 55.818065069402195,
             zoom: 13,
           }}
           style={{ width: width, height: 400 }}
           mapStyle={`https://api.maptiler.com/maps/basic-v2/style.json?key=P5R8144Hukr6UEEJm5NV`}
         >
-          <NavigationControl position="top-left" />
           <Source id="my-data" type="geojson" data={fieldData as any}>
             <Layer {...polygonStyle} />
             <Layer {...borderStyle} />
           </Source>
+          <Marker
+            offset={[0, 0]}
+            longitude={9.924400733409513}
+            latitude={55.818065069402195}
+          >
+            <FaMapMarkerAlt width={25} className="text-blue-600" />
+          </Marker>
         </MapGL>
       </div>
     </PageContainer>
